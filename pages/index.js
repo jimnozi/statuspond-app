a8aa55212cb51b36fbb8816777d1a56451f11fff
jimnozi <1jimnozi@gmail.com>
2025-05-07 20:33:33 +0200

    Use next/image for logo display

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,35 @@
 import { useEffect, useState } from 'react'
+import Image from 'next/image'
+import clients from '../lib/clientData'
 
 export default function Home() {
-  const [clientName, setClientName] = useState('')
+  const [clientData, setClientData] = useState(null)
 
   useEffect(() => {
-    const sub = window.location.hostname.split('.')[0]
-    setClientName(sub)
+    const subdomain = window.location.hostname.split('.')[0]
+    const data = clients[subdomain] || clients.default
+    setClientData(data)
   }, [])
+
+  if (!clientData) return <p>Loading...</p>
 
   return (
     <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
-      <h1>Welcome to {clientName}&apos;s status page!</h1>
-      <p>This is the public status page for <strong>{clientName}</strong>.</p>
+      <Image
+        src={clientData.logo}
+        alt={clientData.name}
+        width={100}
+        height={40}
+        unoptimized // needed for external image URLs
+      />
+      <h1>{clientData.name}&apos;s Status Page</h1>
+      <p>{clientData.status}</p>
+      <p>
+        Need help? Email{' '}
+        <a href={`mailto:${clientData.supportEmail}`}>
+          {clientData.supportEmail}
+        </a>
+      </p>
     </div>
   )
 }
