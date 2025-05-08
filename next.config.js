af1c95ca406986db9cb5951a28464a889f08f153
jimnozi <1jimnozi@gmail.com>
2025-05-08 15:53:13 +0200

    added logs

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      domains: ['vmypfazaedqugjiivhed.supabase.co'], // e.g. abcdxyz.supabase.co
+    },
+  }
+  
+  module.exports = nextConfig
+  
